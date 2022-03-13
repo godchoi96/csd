3a6b655a8246f6d24a19f9b5666758447eb00f32
godChoi96 <oinkoink71@naver.com>
2022-03-13 18:19:44 +0900

    Question - 최대공약수, 최소공배수

---
+++ prac5.py
@@ -0,0 +1,19 @@
+def solution(n, m):
+    answer = []
+    temp = 0
+    a = n
+    b = m
+    while True:
+        if a % b != 0: # 최대공약수를 구하는 식은 유클리드 호제법을 사용
+            temp = a % b # a    b    나머지
+            a = b        # 1071 1029 42 
+            b = temp     # 1029 42   21
+        else:            # 42   21   0
+            break
+    answer.append(b)
+    answer.append(int((n*m)/b))
+    return answer
+
+# 디버깅하는 곳입니다.
+print(solution(3, 12))
+print(solution(2, 5))
