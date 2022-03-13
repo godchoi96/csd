79f902c61528de9445352eda0e1ed96a321b9c31
godChoi96 <oinkoink71@naver.com>
2022-03-13 21:47:02 +0900

    Question - Collatz

---
+++ prac6.py
@@ -0,0 +1,29 @@
+def solution(n):
+    answer = 0
+
+    while True:
+        if answer == 500:
+            answer = -1
+            break
+        
+        if n == 1:
+            answer = 0
+            break
+
+        if n % 2 == 0:
+            n = n / 2
+            answer += 1
+            if n == 1:
+                break
+        else:
+            n = n*3 + 1
+            answer += 1
+            if n == 1:
+                break
+    return answer
+
+# 디버깅하는 곳입니다.
+print(solution(1))
+print(solution(6))
+print(solution(16))
+print(solution(626331))
