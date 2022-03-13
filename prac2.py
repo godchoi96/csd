2f5839c32ab03cf47445f81335a5da5445e3986b
godChoi96 <oinkoink71@naver.com>
2022-03-13 12:34:17 +0900

    Question - List by interval

--- prac2.py
+++ prac2.py
@@ -1,5 +1,9 @@
 def solution(x, n):
     answer = []
+    # 2*1 2*2 2*3 2*4 2*5
+    # 4*1 4*2 4*3
+    # -4*1 -4*2 
+    # x * i 를 리스트로 출력하는 규칙
     for i in range(1, n+1):
         temp = x * i
         answer.append(temp)
