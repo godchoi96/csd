7bfc77ed4c12f00ab28800dd67148b43aae7043e
godChoi96 <oinkoink71@naver.com>
2022-03-13 23:32:34 +0900

    Question - Square Root

---
+++ prac7.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    temp = n ** (1/2)
+    
+    if temp % 1 == 0: # 소수점이 있는 수를 1로 나누면 소수점이 나오고, 11.0과 같은 형태는 0.0이 나오게 된다.
+        answer = int((temp + 1) ** 2)
+    else:
+        answer = -1
+
+    return answer
+
+# 디버깅하는 곳입니다.
+print(solution(121))
+print(solution(3))
