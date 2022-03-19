e9684200e580b225b18178a54c63ed722ff17e17
godChoi96 <oinkoink71@naver.com>
2022-03-20 02:37:53 +0900

    220318 answer / 220320 solved

--- morning_test/220318.py
+++ morning_test/220318.py
@@ -1,4 +1,22 @@
 # 1. 더하기 사이클
+def addCycle(n):
+    check = n # 미리 값을 저장하기 위한 변수
+    sum = 0 # 각 자릿 수의 합
+    change_num = 0 # 바뀐 숫자를 저장할 변수
+    cnt = 0 # 바뀐 횟수를 저장할 변수
+
+    while True:
+        sum = n // 10 + n % 10
+        change_num = 10 * (n % 10) + sum % 10
+        cnt += 1
+        n = change_num
+        if change_num == check:
+            break
+    return cnt
+
+print(addCycle(26))
+print(addCycle(0))
+print(addCycle(71))
 
 # 2. 평균은 넘겠지
 def averageIs(n, score):
