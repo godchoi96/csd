944041c043cd98a55a7901a2a4f6c8f9ba3d9cf0
godChoi96 <oinkoink71@naver.com>
2022-03-22 04:53:13 +0900

    최빈값 구하기

--- data_structure_theory/part1.py
+++ data_structure_theory/part1.py
@@ -7,6 +7,8 @@ def find_alphabet_occurrence_array(string):
             alphabet_occurrence_array[ord(string[i]) - ord('a')] += 1
         
         answer = chr(alphabet_occurrence_array.index(max(alphabet_occurrence_array)) + ord('a'))
+        # 0 + 97 = 97 / 1 + 97 = 98
+        # 인덱스 번호에서 97을 더하면 해당하는 인덱스 번호의 아스키 코드가 나온다. (현재 위 리스트에서는 빈도수 별로 저장되어 있기 때문)
 
     return answer
 
