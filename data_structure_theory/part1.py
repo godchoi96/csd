56a1ca6d069295f5c7721db005544748426307ee
godChoi96 <oinkoink71@naver.com>
2022-03-22 04:49:34 +0900

    최빈값 구하기

--- data_structure_theory/part1.py
+++ data_structure_theory/part1.py
@@ -3,11 +3,11 @@ def find_alphabet_occurrence_array(string):
     alphabet_occurrence_array = [0] * 26
 
     for i in range(len(string)):
-        if string[i] != ' ':
+        if string[i].isalpha():
             alphabet_occurrence_array[ord(string[i]) - ord('a')] += 1
+        
+        answer = chr(alphabet_occurrence_array.index(max(alphabet_occurrence_array)) + ord('a'))
 
-    return max(alphabet_occurrence_array)
+    return answer
 
 print(find_alphabet_occurrence_array("hello my name is sparta"))
-    
-# 2. 
