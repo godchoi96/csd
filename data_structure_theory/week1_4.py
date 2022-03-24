df8c038876b7399a6081bb50fea6e2f773b10431
godChoi96 <oinkoink71@naver.com>
2022-03-24 23:40:11 +0900

    곱하기 / 더하기

--- data_structure_theory/week1_4.py
+++ data_structure_theory/week1_4.py
@@ -1,4 +1,7 @@
 # 4. 점근표기법 알아보기
+from re import I
+
+
 def is_number_exist(number, array):
     
     for i in array:
@@ -7,3 +10,15 @@ def is_number_exist(number, array):
     return False
 
 print(is_number_exist(3, [3, 5, 6, 1, 2, 4]))
+
+# 5. 곱하기 / 더하기
+def find_max_plus_or_multiply(array):
+    sum = 0
+    for num in array:
+        if num <= 1 or sum <=1:
+            sum += num
+        else:
+            sum *= num
+    return sum
+
+print(find_max_plus_or_multiply([0, 3, 5, 6, 1, 2, 4]))
