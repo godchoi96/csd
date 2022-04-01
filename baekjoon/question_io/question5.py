6db5fda65a21c45acf7b7c6fa0b20e0b54d6d5fb
godChoi96 <oinkoink71@naver.com>
2022-04-01 11:39:07 +0900

    A + B -4

---
+++ baekjoon/question_io/question5.py
@@ -0,0 +1,9 @@
+# 5. A + B - 4
+# https://www.acmicpc.net/problem/10951
+
+while True:
+    try:
+        num1, num2 = input().split()
+        print(int(num1) + int(num2))
+    except:
+        break
