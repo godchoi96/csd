14258be3fcce5ccbfbc21764962bc739cece5cc9
godChoi96 <oinkoink71@naver.com>
2022-04-03 14:57:57 +0900

    가위바위보

--- goormlevel/level1/question1.py
+++ goormlevel/level1/question1.py
@@ -6,13 +6,17 @@
 user_input = list(map(int, input().split()))
 check = list(set(user_input))
 
+# set으로 정렬한 리스트가 1 또는 3이면 비긴 경우이기 때문에 0을 출력
 if len(check) == 1 or len(check) == 3:
     print(0)
 else:
+    # 1이 없는 케이스 > 2와 3을 비교했을 때 3이 이기기 때문에 입력받은 리스트에서 3의 개수를 계산
     if user_input.count(1) == 0:
         print(user_input.count(3))
+    # 가위 / 보 상황
     elif user_input.count(2) == 0:
         print(user_input.count(1))
+    # 가위 / 바위 상황
     else:
         print(user_input.count(2))
 
