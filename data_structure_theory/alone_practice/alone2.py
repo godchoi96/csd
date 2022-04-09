cfa7f0a8dc5d181746a812b0ba2371c75eab9337
godChoi96 <oinkoink71@naver.com>
2022-04-09 16:56:04 +0900

    링크드 리스ㅌ

---
+++ data_structure_theory/alone_practice/alone2.py
@@ -0,0 +1,28 @@
+# 1. 링크드 리스트
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+    
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+def list_sum(list):
+    num_sum = 0
+    head = list.head
+    while head is not None:
+        num_sum = num_sum * 10 + head.data
+        head = head.next
+    return num_sum
+
+def two_list_sum(list1, list2):
+    num_sum1 = list_sum(list1)
+    num_sum2 = list_sum(list2)
+    return num_sum1 + num_sum2
