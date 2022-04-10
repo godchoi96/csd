02e779bef4625bb4bee19c2c546a377031e53747
godChoi96 <oinkoink71@naver.com>
2022-04-11 00:04:14 +0900

    사칙연산

---
+++ baekjoon/question30~39/question33.py
@@ -0,0 +1,10 @@
+# 33. 사칙연산
+# https://www.acmicpc.net/problem/10869
+
+num1, num2 = map(int, input().split())
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
+print(num1 % num2)
