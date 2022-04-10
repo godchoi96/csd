829cc332ab96986cd58a96939b1696b4f7609dc0
godChoi96 <oinkoink71@naver.com>
2022-04-10 16:11:42 +0900

    버블정렬 - 재귀함수

--- data_structure_theory/sparta/week3_1.py
+++ data_structure_theory/sparta/week3_1.py
@@ -13,3 +13,14 @@ def bubble_sort(array):
     return array
 
 print(bubble_sort([4, 6, 2, 9, 1]))
+
+# 2. 혼자서 해보는 재귀함수 버블정렬
+def bubble_sort_recur(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            array[i], array[i + 1] = array[i + 1], array[i]
+    if array == sorted(array):
+        return array
+    return bubble_sort_recur(array)
+
+print(bubble_sort_recur([4, 6, 2, 9, 1]))
