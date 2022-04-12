386b3df435d57fa2592d4f7996982eab8cc682cd
godChoi96 <oinkoink71@naver.com>
2022-04-12 13:32:45 +0900

    사분면 고르기

---
+++ baekjoon/question40~49/question41.py
@@ -0,0 +1,16 @@
+# 41. 사분면 고르기
+# https://www.acmicpc.net/problem/14681
+
+num1 = int(input())
+num2 = int(input())
+
+if num1 > 0:
+    if num2 > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if num2 > 0:
+        print(2)
+    else:
+        print(3)
