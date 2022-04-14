4e753babb47632d6a76c3e2bbb16019425560516
godChoi96 <oinkoink71@naver.com>
2022-04-14 13:45:03 +0900

    셀프 넘버

---
+++ baekjoon/question50~59/question55.py
@@ -0,0 +1,18 @@
+# 55. 셀프 넘버
+# https://www.acmicpc.net/problem/4673
+
+full_list = set(range(1, 10001))
+list = []
+
+def non_make_self_number(num):
+    for n in str(num):
+        num += int(n)
+    return num
+
+for i in range(1, 10000):
+    list.append(non_make_self_number(i))
+
+list = set(list)
+
+for number in sorted(full_list - list):
+    print(number)
