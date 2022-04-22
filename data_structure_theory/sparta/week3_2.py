3f2cdf1787adae886753220e6edba41aede1b492
godChoi96 <oinkoink71@naver.com>
2022-04-22 09:10:04 +0900

    삽입정렬

--- data_structure_theory/sparta/week3_2.py
+++ data_structure_theory/sparta/week3_2.py
@@ -15,3 +15,18 @@ def selection_sort(array):
     return array
 
 print(selection_sort([4, 6, 2, 9, 1]))
+
+#2. 삽입정렬
+def insertion_sort(array):
+    # - -
+    # - - -
+    # - - - -
+    # - - - - -
+    for i in range(1, len(array)):
+        for j in range(i):
+            if array[i] < array[j]:
+                array[j], array[i] = array[i], array[j]
+            print(array)
+    return array
+
+print(selection_sort([4, 6, 2, 9, 1]))
