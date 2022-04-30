bcd9d68de0e46798ef286139cd99c0db03ec3030
godChoi96 <oinkoink71@naver.com>
2022-04-30 13:00:14 +0900

    소수 찾기

---
+++ baekjoon/question70~79/question73.py
@@ -0,0 +1,11 @@
+# 73. 소수 찾기
+# https://www.acmicpc.net/problem/1978
+
+N = int(input())
+input_list = []
+num_list = []
+
+for i in range(N):
+    input_list.append(int(input()))
+
+max_num = max(input_list)
