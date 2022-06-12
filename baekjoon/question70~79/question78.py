5c22842066b1dd87fb6f0e48732e7f3cde5b96e2
godChoi96 <oinkoink71@naver.com>
2022-06-13 00:51:11 +0900

    골드바흐의 추측

--- baekjoon/question70~79/question78.py
+++ baekjoon/question70~79/question78.py
@@ -6,39 +6,68 @@
 반복되는 로직은?
 """
 
-num = int(input())
-boolean_list = [True] * (num + 1)
-boolean_index_list = []
-answer = []
-test = []
+# from xmlrpc.client import boolean
 
-for i in range(2, int(num ** 0.5) + 1):
-    if boolean_list[i] == True:
-        for j in range(i * 2, num + 1, i):
-            boolean_list[j] = False
 
-for i in range(len(boolean_list)):
-    if boolean_list[i] == True:
-        boolean_index_list.append(i)
+# num = int(input())
+# boolean_list = [True] * (num + 1)
+# boolean_index_list = []
+# answer = []
+# test = []
 
-boolean_index_list = boolean_index_list[2:]
-print(boolean_index_list)
+# for i in range(2, int(num ** 0.5) + 1):
+#     if boolean_list[i] == True:
+#         for j in range(i * 2, num + 1, i):
+#             boolean_list[j] = False
 
-"""
-0 0 0 1 0 2 0 3
-1 1 1 2 1 3
-2 2 2 3
-3 3
-"""
+# for i in range(len(boolean_list)):
+#     if boolean_list[i] == True:
+#         boolean_index_list.append(i)
 
-for i in range(len(boolean_index_list)):
-    for j in range(i, len(boolean_index_list)):
-        if boolean_index_list[i] + boolean_index_list[j] == num:
-            answer.append([boolean_index_list[i], boolean_index_list[j]])
+# boolean_index_list = boolean_index_list[2:]
+# print(boolean_index_list)
 
-print(answer[-1][0], answer[-1][1])
+# """
+# 0 0 0 1 0 2 0 3
+# 1 1 1 2 1 3
+# 2 2 2 3
+# 3 3
+# """
+
+# for i in range(len(boolean_index_list)):
+#     for j in range(i, len(boolean_index_list)):
+#         if boolean_index_list[i] + boolean_index_list[j] == num:
+#             answer.append([boolean_index_list[i], boolean_index_list[j]])
+
+# print(answer[-1][0], answer[-1][1])
 
 """
 해결했지만 런타임 에러가 발생.
+문제를 보면 소수가 아닌 수를 입력 받으면 다음 수를 입력받게 만드는 모양이다. 
+그리고 입력받은 수를 반 나눠서 하나는 +1, 다른 하나는 -1 하면서 입력받은 수랑 같으면 탈출시키면 되는 구조였다.
 """
 
+def is_prime(n):
+    if n == 1:
+        return False
+    
+    for j in range(2, int(n ** 0.5) + 1):
+        if n %  j == 0:
+            return False
+    
+    return True
+
+T = int(input())
+for i in range(T):
+    num = int(input())
+
+    if not(num % 2):
+        a, b = num // 2, num // 2
+        
+        while a > 0:
+            if is_prime(a) and is_prime(b):
+                print(a, b)
+                break
+            else:
+                a -= 1
+                b += 1
