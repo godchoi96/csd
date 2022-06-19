ac78dfe147eb8d66c5256f8ffdfb0107b6169bb8
godChoi96 <oinkoink71@naver.com>
2022-06-19 16:39:12 +0900

    2022.06.19 - 자바의 구조

---
+++ java/src/Structure.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Structure {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
+
+/* 자바의 구조 */
+/*
+    public class 클래스명 {
+        [public|private|protected] [static] (리턴 자료형|void) 메소드명(입력자료형, 매개변수, ...) {
+            명령문(statement)
+        }
+    }
+*/
