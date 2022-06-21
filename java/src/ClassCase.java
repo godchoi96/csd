cae163347fdb15b7c415eb98ac31150e8a115d74
godChoi96 <oinkoink71@naver.com>
2022-06-21 13:02:22 +0900

    2022.06.21 - Class

--- java/src/ClassCase.java
+++ java/src/ClassCase.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ClassCase {
+class Introduce {
+    String name;
+    public void introduceName(String name) {
+        this.name = name;
+    }
+}
+
+public class ClassCase {
+    public static void main(String[] args) {
+        Introduce introduce = new Introduce();
+        introduce.introduceName("ChoiSeongdae");
+        System.out.println(introduce.name);
+    }
 }
