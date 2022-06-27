6dd804e24ef067b426b50a6d316ce653e465c1e0
godChoi96 <oinkoink71@naver.com>
2022-06-27 19:46:39 +0900

    2022.06.27 - Constructor

---
+++ java/src/ConstructorCase.java
@@ -0,0 +1,42 @@
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Dog extends Animal {
+    void sleep() {
+        System.out.println(this.name + " zzz");
+    }
+}
+
+class HouseDog extends Dog {
+    HouseDog(String name) {
+        this.setName(name);
+    }
+
+    HouseDog(int type) {
+        if (type == 1) {
+            this.setName("yorkshire");
+        } else if (type == 2) {
+            this.setName("bulldog");
+        }
+    }
+
+    void sleep() {
+        System.out.println(this.name + " zzz in house");
+    }
+
+    void sleep(int hour) {
+        System.out.println(this.name + " zzz in house for " + hour + " hours");
+    }
+}
+
+public class ConstructorCase {
+    public static void main(String[] args) {
+        HouseDog dog = new HouseDog("Test");
+        System.out.println(dog.name);
+    }
+}
