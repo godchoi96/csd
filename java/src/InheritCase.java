5a3fbb133f128ec623f23a9026be81ee76a006a0
godChoi96 <oinkoink71@naver.com>
2022-06-27 19:19:20 +0900

    2022.06.27 - Inheritacne

---
+++ java/src/InheritCase.java
@@ -0,0 +1,33 @@
+class Program {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Django extends Program {
+    void coding() {
+        System.out.println(this.name + " start");
+    }
+}
+
+class JSP extends Django {
+    void coding() {
+        System.out.println(this.name + " start in JSP");
+    }
+
+    void coding(String code) {
+        System.out.println(this.name + " start in JSP by " + code);
+    }
+}
+
+public class InheritCase {
+    public static void main(String[] args) {
+        JSP jsp = new JSP();
+        jsp.setName("django");
+        System.out.println(jsp.name);
+        jsp.coding();
+        jsp.coding("Java");
+    }
+}
