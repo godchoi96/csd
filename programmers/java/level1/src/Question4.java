d91e6ea3146eeebacd4df59416a68b317e972cc9
godChoi96 <oinkoink71@naver.com>
2022-07-04 07:41:07 +0900

    2022.07.04 - 핸드폰 번호 가리기

---
+++ programmers/java/level1/src/Question4.java
@@ -0,0 +1,17 @@
+public class Question4 {
+    public String solution(String phone_number) {
+        String answer = "";
+        String temp = phone_number.substring(phone_number.length() - 4, phone_number.length());
+
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            answer += '*';
+        }
+        answer += temp;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Question4 q4 = new Question4();
+        System.out.println(q4.solution("01033334444"));
+        System.out.println(q4.solution("027778888"));
+    }
+}
